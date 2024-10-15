87478ebdc90deada7518bb165ea060fbb85d1210
Sanjyot Kakade <sanjyotkakade9@gmail.com>
2024-10-15 15:49:50 +0530

    sanjyot

---
+++ ReactWebFront/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo"></div>
+        <div className="gallery">Gallery</div>
+        <div className="facilitis"></div>
+        <div className="aboutUs"></div>
+        <div className="contact"></div>
+        <div className="loginLogo"></div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
