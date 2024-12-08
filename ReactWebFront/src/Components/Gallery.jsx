6e367b3c00b63d57e274564507e211d99874a998
Sanjyot Kakade <sanjyotkakade9@gmail.com>
2024-12-09 00:48:59 +0530

    change\

--- ReactWebFront/src/Components/Gallery.jsx
+++ ReactWebFront/src/Components/Gallery.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import img1 from '../assets/images/Hero1.png'
+import React from 'react';
+import img1 from '../assets/images/hos1.jpg';
+import img2 from '../assets/images/hos2.jpg';
+import img3 from '../assets/images/hos3.jpg';
+import img4 from '../assets/images/hos4.jpg';
 
-const Images =(props)=>{
-  return <div className="1">
-      <img className='h-[400px] w-[350px] rounded-xl object-cover' src={img1} alt="" />
+const Images = (props) => {
+  return (
+    <div className="relative overflow-hidden rounded-xl"> {/* Apply rounded corners to the container */}
+      <img
+        className=" rounded-t-[50px] h-[400px] w-[350px] object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+        src={props.img}
+        alt=""
+      />
     </div>
-}
+  );
+};
 
 function Gallery() {
   return (
-    <div className='h-screen  mt-[200px] bg-sky-100 w-full rounded-t-[50px] p-5 '>
-      <h1 className='font-serif text-7xl mt-[100px] flex justify-start'>Gallery</h1>
-      
-      <div className=' gap-4 mt-8 flex-wrap p-5 flex justify-center items-center '>
-            <Images/>
-            <Images/>
-            <Images/>
-            <Images/>
+    <div className="h-screen mt-[200px] bg-sky-100 w-full rounded-t-[50px] p-5">
+      <h1 className="font-serif text-7xl mt-[100px] flex justify-start">Gallery</h1>
+      <div className="gap-4 mt-8 flex-wrap p-5 flex justify-center items-center">
+        <Images img={img1} />
+        <Images img={img2} />
+        <Images img={img3} />
+        <Images img={img4} />
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
